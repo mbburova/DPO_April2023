af3d962874945dd9c3cf288be9990b3c9e5ce111
Margarita Burova <mbburova@gmail.com>
2023-06-24 21:55:31 +0300

    Update main.py

--- Lect18/main.py
+++ Lect18/main.py
@@ -1,17 +1,1 @@
-from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    greet_with_link = """<h1>Привет, Мир!</h1>
-    <p><a href="user/Аникин/Георгий">Нажми на меня</a></p>"""
-    return greet_with_link
-
-@app.route('/user/<surname>/<name>')
-def get_user(name, surname):
-    personal_instruc = f"""<h1>Привет, {surname} {name}!</h1>
-    <p>Измени имя пользователя в адресной строке и перезагрузи страницу</p>"""
-    return personal_instruc
-
-if __name__ == '__main__':
-    app.run(debug=True)
